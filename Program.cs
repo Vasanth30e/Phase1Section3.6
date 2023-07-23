2b9a9032f753a2bda618e36b0999bf50af3140ab
unknown <vasanthgamer2000@gmail.com>
2023-07-23 17:33:10 +0530

    Control Flow

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section3._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            doApp();
+            Console.ReadKey();
+        }
+        public static void doApp()
+        {
+            int x = 10;
+
+            if (x >= 10)
+                Console.WriteLine("This is a conditional statement.");
+            switch (x) 
+            {
+                case 9:
+                    Console.WriteLine("This is a switch statement resulting in 9");
+                    break;
+                case 10:
+                    Console.WriteLine("This is a switch statement resulting in 10");
+                    break;
+
+            } 
+            while(x < 20)
+            {
+                x++;
+                Console.WriteLine("Incrementing x in while loop: " + x);
+            }
+
+            do
+            {
+                x--;
+                Console.WriteLine("Decrementing x in do-while loop: " + x);
+
+            } while (x >= 10);
+
+            for(int i = 0; i <= x; i++)
+            {
+                Console.WriteLine("For loop to assign a value: " + i);
+            }
+
+            string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+            foreach(string s in numbers)
+            {
+                Console.WriteLine("Foreach loop to iterate through an array: " + s);
+            }
+
+        }
+    }
+}
